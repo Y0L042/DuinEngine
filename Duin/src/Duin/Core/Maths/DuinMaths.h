85eb4265e8d20c18cd500ca08362a4082b0acd88
Y0L042@DinkBoekWin11 <weweresomewhere@gmail.com>
2025-06-30 16:48:43 +0200

    Swap TOWARD and AWAY constants in Vector3 and Vector4
    Updated the `TOWARD` and `AWAY` constants in both the `Vector3` and `Vector4` classes in `DuinMaths.cpp` to reflect correct directional values.
    Additionally, added a comment in `DuinMaths.h` to clarify the calculation method used in the `Vector3CrossProduct` function, specifying the right-hand rule (RHR) from `V1` to `V2`.

--- Duin/src/Duin/Core/Maths/DuinMaths.h
+++ Duin/src/Duin/Core/Maths/DuinMaths.h
@@ -940,7 +940,7 @@ DNMAPI Vector3 Vector3Multiply(Vector3 v1, Vector3 v2)
     return result;
 }
 
-// Calculate two vectors cross product
+// Calculate two vectors cross product, RHR from V1 to V2
 DNMAPI Vector3 Vector3CrossProduct(Vector3 v1, Vector3 v2)
 {
     Vector3 result = { v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x };
