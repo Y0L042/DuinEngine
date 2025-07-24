981b4ddfc44614e73b5a4c49c9a906860fcc2785
Y0L042@DinkBoekWin11 <weweresomewhere@gmail.com>
2025-07-24 13:08:12 +0200

    Add ECS scene serialization methods to SceneBuilder
    Implemented WriteScene, WriteEntity, and RecursiveWriter methods in SceneBuilder to facilitate the serialization of ECS scene data to a file. Updated the SceneBuilder.h header to reflect these changes and removed the old WriteScene method signature. Added necessary includes for ECS functionality.

--- Duin/src/Duin/ECS/SceneBuilder.cpp
+++ Duin/src/Duin/ECS/SceneBuilder.cpp
@@ -2,7 +2,11 @@
 #include "SceneBuilder.h"
 #include "ECSManager.h"
 #include "Duin/IO/DataValue.h"
+#include <flecs.h>
 
+/**
+ * Reads an ECS scenetree from file and generates the scenetree.
+ */
 void duin::SceneBuilder::ReadScene(const std::string& file, ECSManager& ecs)
 {
     DataValue root(file);
@@ -11,7 +15,54 @@ void duin::SceneBuilder::ReadScene(const std::string& file, ECSManager& ecs)
     e_root.is_a(ecs.world.lookup("TYPE"));
 }
 
-void duin::SceneBuilder::WriteScene(const std::string& file, const std::string& scene)
+void duin::SceneBuilder::WriteScene(std::string& file, ECSManager& ecs)
 {
+    DataValue sceneTree;
+    DataValue children;
+    children.SetArray();
 
+    /* Loop through each entity in world root */
+    ecs.world.children([&](flecs::entity e) {
+        DataValue childData;
+        RecursiveWriter(e, childData);
+        children.PushBack(childData);
+        });
+    sceneTree.AddMember("Children", children);
+
+    file = DataValue::Write(sceneTree, true);
+}
+
+void duin::SceneBuilder::WriteEntity(std::string& file, flecs::entity entity)
+{
+    DataValue tree;
+    DataValue children;
+
+    /* Loop through root entity's children */
+    children.SetArray();
+    entity.children([&](flecs::entity e) {
+        DataValue childData;
+        RecursiveWriter(e, childData);
+        children.PushBack(childData);
+        });
+    tree.AddMember("Children", children);
+
+    file = DataValue::Write(tree, true);
+}
+
+void duin::SceneBuilder::RecursiveWriter(flecs::entity entity, DataValue& entityData)
+{
+    // Serialise entity
+    DataValue ecsID((int)entity);
+    DataValue children;
+
+    entityData.AddMember("ECS_ID", ecsID);
+
+    // Loop through children
+    children.SetArray();
+    entity.children([&](flecs::entity e) {
+        DataValue childData;
+        RecursiveWriter(e, childData);
+        children.PushBack(childData);
+        }); 
+    entityData.AddMember("Children", children);
 }
--- Duin/src/Duin/ECS/SceneBuilder.h
+++ Duin/src/Duin/ECS/SceneBuilder.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <string>
+#include "Duin/IO/DataValue.h"
 #include "ECSManager.h"
+#include <flecs.h>
 
 namespace duin {
     class SceneBuilder
@@ -9,8 +11,10 @@ namespace duin {
         public:
             // input string must have root entity as json root
             static void ReadScene(const std::string& file, ECSManager& ecs);
-            static void WriteScene(const std::string& file, const std::string& scene);
+            static void WriteScene(std::string& file, ECSManager& ecs);
+            static void WriteEntity(std::string& file, flecs::entity entity);
 
         private:
+            static void RecursiveWriter(flecs::entity e, DataValue& entityData);
     };
 }
