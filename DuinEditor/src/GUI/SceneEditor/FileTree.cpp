fb20831d67e08758d759f124fed43e9483d48e11
Y0L042@DinkBoekWin11 <weweresomewhere@gmail.com>
2026-02-06 14:15:38 +0200

    Integrate FileTree with scene editor and file manager
    Refactored FileTree to accept and track State_SceneEditor and FileManager pointers, connecting to scene editor signals for updates. Replaced placeholder UI with recursive file tree rendering using DrawNode, supporting selection and scene file activation via onSceneSelect signal. Added relevant private members and forward declarations.

--- DuinEditor/src/GUI/SceneEditor/FileTree.cpp
+++ DuinEditor/src/GUI/SceneEditor/FileTree.cpp
@@ -1,6 +1,12 @@
 #include "FileTree.h"
+#include "EditorStates/State_SceneEditor.h"
+#include "FileManager.h"
 
 FileTree::FileTree()
+{
+}
+
+FileTree::FileTree(State_SceneEditor *sceneEditor) : sceneEditor(sceneEditor)
 {
 }
 
@@ -8,9 +14,23 @@ FileTree::~FileTree()
 {
 }
 
+void FileTree::SetSceneEditor(State_SceneEditor *sceneEditor)
+{
+    this->sceneEditor = sceneEditor;
+}
+
+void FileTree::SetFileManager(FileManager *fm)
+{
+    this->fm = fm;
+}
+
 void FileTree::Init()
 {
     imguiWindowFlags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoCollapse;
+    if (sceneEditor != nullptr)
+    {
+        sceneEditor->onUpdateFileManager.Connect([&](FileManager *fm) { SetFileManager(fm); });
+    }
 }
 
 void FileTree::PhysicsUpdate(double delta)
@@ -24,8 +44,65 @@ void FileTree::Draw()
 void FileTree::DrawUI()
 {
     ImGui::Begin("FileTree", nullptr, imguiWindowFlags);
-
-    ImGui::Text("This is my custom FileTree window content!");
+    if (fm != nullptr)
+    {
+        DrawNode(&fm->GetRootNode(), fm->GetRootNode().name);
+    }
 
     ImGui::End();
+}
+
+void FileTree::DrawNode(FSNode *node, const std::string &nodeLabel)
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+
+    ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick;
+    if (node == selectedNode)
+        flags |= ImGuiTreeNodeFlags_Selected;
+
+    if (node->type == ArcheType::P_DIRECTORY)
+    {
+        // Open a tree node
+        bool nodeOpen = ImGui::TreeNodeEx(nodeLabel.c_str(), flags);
+
+        if (ImGui::IsItemClicked())
+        {
+            selectedNode = node;
+        }
+
+        if (nodeOpen)
+        {
+            for (FSNode *child : node->subNodes)
+            {
+                DrawNode(child, child->name);
+            }
+            ImGui::TreePop();
+        }
+    }
+    else
+    {
+        ImGuiTreeNodeFlags leafFlags = flags | ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen;
+        ImGui::TreeNodeEx(nodeLabel.c_str(), leafFlags);
+        if (ImGui::IsItemClicked())
+        {
+            selectedNode = node;
+        }
+
+        if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(0))
+        {
+            selectedNode = node;
+            if (node->type == ArcheType::P_FILE && node->fileExt == FILEEXT_ECST)
+            {
+                onSceneSelect.Emit(node);
+            }
+        }
+
+        if (ImGui::IsItemHovered() && ImGui::IsMouseClicked(1))
+        {
+            selectedNode = node;
+        }
+    }
 }
--- DuinEditor/src/GUI/SceneEditor/FileTree.h
+++ DuinEditor/src/GUI/SceneEditor/FileTree.h
@@ -1,18 +1,34 @@
 #pragma once
 #include <external/imgui.h>
 #include <Duin/Objects/ObjectModule.h>
+#include <Duin/Core/Signals/Signal.h>
 #include <memory>
 
+class State_SceneEditor;
+class FileManager;
+class FSNode;
 class FileTree : public duin::GameObject
 {
   public:
+    duin::Signal<FSNode *> onSceneSelect;
+
     FileTree();
+    FileTree(State_SceneEditor *sceneEditor);
     ~FileTree();
 
     void Init() override;
     void PhysicsUpdate(double delta) override;
     void Draw() override;
     void DrawUI() override;
+
+    void SetSceneEditor(State_SceneEditor *sceneEditor);
+    void SetFileManager(FileManager *fm);
+
+    void DrawNode(FSNode *node, const std::string &nodeLabel);
+
   private:
-    ImGuiWindowFlags imguiWindowFlags;
+    ImGuiWindowFlags imguiWindowFlags = 0;
+    State_SceneEditor *sceneEditor = nullptr;
+    FileManager *fm = nullptr;
+    FSNode *selectedNode = nullptr;
 };
