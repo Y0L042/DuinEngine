94d2fc5acc429780c32811131357efd4629f7a59
Y0L042@DinkBoekWin11 <weweresomewhere@gmail.com>
2026-02-06 14:14:53 +0200

    Add SceneDefinition and ExternalSceneDependency classes
    Introduce SceneDefinition and ExternalSceneDependency to represent scenes and their external dependencies, with JSON-based constructors and relevant member variables. Define string constants for JSON keys and update includes and namespace usage.

---
+++ Duin/src/Duin/Scene/SceneDefinition.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Duin/IO/JSONValue.h"
+#include "Duin/Core/Utils/UUID.h"
+
+#include <string>
+#include <vector>
+
+namespace duin
+{
+class ExternalSceneDependency;
+class SceneDefinition
+{
+  public:
+    SceneDefinition() = default;
+    SceneDefinition(const JSONValue &json);
+
+    UUID uuid = UUID::INVALID;
+    std::string name = "";
+    std::vector<ExternalSceneDependency> extScnDeps;
+    std::string flecsJSON = "";
+
+  private:
+    friend class ExternalSceneDependency;
+};
+
+class ExternalSceneDependency
+{
+  public:
+    ExternalSceneDependency() = default;
+    ExternalSceneDependency(const JSONValue &json);
+
+    UUID exDepUUID = UUID::INVALID;
+    uint64_t localParent = 0;
+    std::string type = "";
+
+  private:
+    friend class SceneDefinition;
+};
+} // namespace duin
