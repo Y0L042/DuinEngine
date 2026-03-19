158da0a358a5ae388d1f3f2d4ba07cc73689f253
Y0L042@DinkBoekWin11 <weweresomewhere@gmail.com>
2026-03-19 11:53:26 +0200

    Remove asset index logic from SceneBuilder::Enter()
    Removed all code related to asset index creation, population, serialization, and deserialization from SceneBuilder::Enter(). Also cleaned up unused includes and the static INDEX_PATH variable. The Enter() method is now empty.

--- ExampleProjects/Sandbox/src/Sandboxes/Scenes/SceneBuilder.cpp
+++ ExampleProjects/Sandbox/src/Sandboxes/Scenes/SceneBuilder.cpp
@@ -1,12 +1,8 @@
 #include "SceneBuilder.h"
-#include <Duin/Assets/AssetIndex.h>
 #include <Duin/Core/Debug/DNLog.h>
 #include <Duin/Core/Debug/DNAssert.h>
-#include <Duin/Assets/FileTypes.h>
 
 #include <string>
-
-static const std::string INDEX_PATH = "";
 
 SceneBuilder::SceneBuilder(duin::GameStateMachine &sm) : duin::GameState(sm)
 {
@@ -18,32 +14,6 @@ SceneBuilder::~SceneBuilder()
 
 void SceneBuilder::Enter()
 {
-    duin::AssetIndex assetIndex;
-
-    auto &index = assetIndex.GetIndex();
-    
-    for (int i = 0; i < 150; i++)
-    {
-        duin::AssetRef aref;
-        aref.uuid = duin::UUID(); // default ctor generates random UUID
-        aref.hintPath = "assets/item_" + std::to_string(i);
-        aref.fileType = static_cast<duin::FileType>(std::rand() % duin::FS_FILETYPE_COUNT);
-        aref.fileExt = static_cast<duin::FileExt>(std::rand() % duin::FS_FILEEXT_COUNT);
-        assetIndex.AddAsset(aref);
-    }
-
-    std::string output;
-    assetIndex.Serialize(output);
-    DN_INFO(output);
-
-    duin::AssetIndex assetIndex2;
-    assetIndex2.Deserialize(output);
-    assetIndex2.AddAsset({"assets/item_420", duin::FS_FILETYPE_MODEL_EXT, duin::FS_FILEEXT_BLEND});
-    std::string output2;
-    assetIndex2.Serialize(output2);
-    DN_INFO("\n\n\n{}\n\n\n{}\n\n\n", output, output2);
-    //DN_ASSERT(output == output2, "Output_1 {}\n\n\n must match Output_2!");
-
 }
 
 void SceneBuilder::OnEvent(duin::Event e)
