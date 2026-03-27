e2e5ce246d8c7a4e038fc65585f11b3da09b3cf2
Y0L042@DinkBoekWin11 <weweresomewhere@gmail.com>
2026-03-27 14:41:19 +0200

    Initial Duin FPS project setup with Daslang scripting
    Added build config, C++ entry point, Daslang scripts, and initial scene files. Sets up hot-reloadable scripting, player/camera entities, and project structure for a modular FPS game using the Duin engine.

---
+++ ExampleProjects/DuinFPSDaslang/premake5.lua
@@ -0,0 +1,89 @@
+SolutionRoot = "../.."
+ProjectRoot = "."
+
+project "DuinFPSDaslang"
+    location ""
+    kind "ConsoleApp"
+    language "C++"
+
+    externalanglebrackets "On"
+    linkoptions { "-IGNORE:4006", "/STACK:8388608" }
+    externalwarnings    "Off"
+
+    dependson { "Duin" }
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    defines(global_defines)
+    defines
+    {
+        "DAS_ENABLE_DYN_INCLUDES=1",
+        "DAS_ENABLE_EXCEPTIONS=1",
+        "DAS_SMART_PTR_DEBUG=1",
+    }
+
+    files
+    {
+        "./src/**.h",
+        "./src/**.hpp",
+        "./src/**.c",
+        "./src/**.cpp",
+    }
+
+    includedirs(prependRoot(SolutionRoot, global_includedirs))
+    includedirs
+    {
+        ProjectRoot .. "/src",
+    }
+
+    externalincludedirs(prependRoot(SolutionRoot, global_externalincludedirs))
+    externalincludedirs
+    {
+        SolutionRoot .. "/" .. IncludeDir["daslang"],
+    }
+
+    libdirs(prependRoot(SolutionRoot, global_libdirs))
+    libdirs
+    {
+        SolutionRoot .. "/Duin/vendor/daslang/lib/Debug",
+    }
+
+    links(global_links)
+    links
+    {
+        "libDaScript.lib",
+        "libUriParser.lib",
+        "dbghelp.lib",
+    }
+
+    postbuildcommands
+    {
+        '{COPYDIR} "' .. path.getabsolute("scripts") .. '" "%{cfg.targetdir}/scripts"',
+        '{COPYDIR} "' .. path.getabsolute("data") .. '" "%{cfg.targetdir}/data"',
+    }
+
+    filter "system:windows"
+        cppdialect "C++20"
+
+    filter "action:vs*"
+        buildoptions {
+            "/utf-8",
+            "/bigobj",
+            '/Zc:__cplusplus',
+            '/Zc:preprocessor',
+        }
+        flags { "MultiProcessorCompile" }
+    filter {}
+
+    filter "configurations:Debug"
+        defines "DN_DEBUG"
+        symbols "On"
+        staticruntime "On"
+        runtime "Debug"
+
+    filter "configurations:Release"
+        defines "DN_RELEASE"
+        optimize "On"
+        staticruntime "On"
+        runtime "Release"
---
+++ ExampleProjects/DuinFPSDaslang/src/main.cpp
@@ -0,0 +1,72 @@
+#include <daScript/daScript.h>
+
+#include <Duin.h>
+#include <Duin/EntryPoint.h>
+#include <Duin/ECS/GameWorld.h>
+#include <Duin/Scene/SceneBuilder.h>
+#include <Duin/IO/FileModule.h>
+#include <Duin/Script/GameScript.h>
+#include <Duin/Script/ScriptModules.h>
+
+#include <iostream>
+
+class DuinFPSDaslangApp : public duin::Application
+{
+    const std::string ENTRY_SCRIPT = "scripts/main.das";
+    std::shared_ptr<duin::GameScript> mainScript;
+
+    int64_t mainModifyTime = 0;
+
+    void Initialize() override
+    {
+        duin::SetFramerate(244);
+        SetWindowStartupSize(1600, 900);
+        SetWindowName("DuinFPS (Daslang)");
+    }
+
+    void Ready() override
+    {
+        mainScript = CreateChildObject<duin::GameScript>(ENTRY_SCRIPT);
+        mainScript->InitModules([]() {
+            NEED_MODULE(Module_Doctest);
+            NEED_MODULE(Module_DnLog);
+            NEED_MODULE(Module_DnGameObject);
+            NEED_MODULE(Module_DecsWorld);
+        });
+        mainScript->EnableHotCompile(true, true);
+        mainScript->CompileAndSimulate();
+    }
+
+    void Update(double delta) override
+    {
+        if (duin::Input::IsKeyPressed(DN_SCANCODE_F5))
+        {
+            mainScript->CompileAndSimulate();
+        }
+    }
+
+    void PhysicsUpdate(double delta) override
+    {
+    }
+
+    void Draw() override
+    {
+    }
+
+    void DrawUI() override
+    {
+    }
+
+    void Exit() override
+    {
+        mainScript->Exit();
+    }
+};
+
+duin::Application *duin::CreateApplication()
+{
+#ifdef DN_DEBUG
+    duin::fs::SetBinDebugMode(true);
+#endif
+    return new DuinFPSDaslangApp();
+}
