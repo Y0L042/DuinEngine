63e1e13282f90597ab14e28f23e3db7c8df7fb49
Y0L042@DinkBoekWin11 <weweresomewhere@gmail.com>
2026-03-27 14:23:15 +0200

    Add dn_log dasScript module for DNLog integration
    Introduced a new dasScript module, dn_log, that integrates the Duin/Core/Debug/DNLog logging system with dasScript. Added Module_DnLog.h and Module_DnLog.cpp to register logging functions (fatal, error, warn, info, trace) as externs, providing script location context in log messages. Included dn_log.das and dn_log.das.inc to configure and embed the module. This enables dasScript scripts to use DNLog logging with enhanced context.

---
+++ Duin/src/Duin/Script/DNLog/Module_DnLog.cpp
@@ -0,0 +1,69 @@
+#include "Module_DnLog.h"
+
+#include <daScript/ast/ast_typefactory_bind.h>
+#include <daScript/ast/ast_interop.h>
+
+#include "dn_log.das.inc"
+
+static std::string dn_log_location(const das::LineInfoArg *at, das::Context *ctx = nullptr)
+{
+    if (at == nullptr)
+    {
+        return "?";
+    }
+   
+    if ((at->fileInfo != nullptr) && (at->fileInfo->tabSize >= 0) && (at->fileInfo->tabSize <= 1000) &&
+        (at->fileInfo->name.size() > 0))
+    {
+        std::string desc = at->describe();
+        return desc;
+    }
+
+    return std::to_string(at->line) + ":" + std::to_string(at->column);
+}
+
+static void dn_log_fatal(const char *msg, das::LineInfoArg *at)
+{
+    DN_FATAL("Script [{}]: \t{}", dn_log_location(at), msg ? msg : "");
+}
+static void dn_log_error(const char *msg, das::LineInfoArg *at)
+{
+    DN_ERROR("Script [{}]: \t{}", dn_log_location(at), msg ? msg : "");
+}
+static void dn_log_warn(const char *msg, das::LineInfoArg *at)
+{
+    DN_WARN("Script [{}]: \t{}", dn_log_location(at), msg ? msg : "");
+}
+static void dn_log_info(const char *msg, das::LineInfoArg *at)
+{
+    DN_INFO("Script [{}]: \t{}", dn_log_location(at), msg ? msg : "");
+}
+static void dn_log_trace(const char *msg, das::LineInfoArg *at)
+{
+    DN_TRACE("Script [{}]: \t{}", dn_log_location(at), msg ? msg : "");
+}
+
+class Module_DnLog : public das::Module
+{
+  public:
+    Module_DnLog() : das::Module("dn_log")
+    {
+        das::ModuleLibrary lib(this);
+        lib.addBuiltInModule();
+
+        das::addExtern<DAS_BIND_FUN(dn_log_fatal)>(*this, lib, "dn_fatal", das::SideEffects::modifyExternal,
+                                                   "dn_log_fatal");
+        das::addExtern<DAS_BIND_FUN(dn_log_error)>(*this, lib, "dn_error", das::SideEffects::modifyExternal,
+                                                   "dn_log_error");
+        das::addExtern<DAS_BIND_FUN(dn_log_warn)>(*this, lib, "dn_warn", das::SideEffects::modifyExternal,
+                                                  "dn_log_warn");
+        das::addExtern<DAS_BIND_FUN(dn_log_info)>(*this, lib, "dn_info", das::SideEffects::modifyExternal,
+                                                  "dn_log_info");
+        das::addExtern<DAS_BIND_FUN(dn_log_trace)>(*this, lib, "dn_trace", das::SideEffects::modifyExternal,
+                                                   "dn_log_trace");
+
+        compileBuiltinModule("dn_log.das", dn_log_das, sizeof(dn_log_das));
+    }
+};
+
+REGISTER_MODULE(Module_DnLog);
---
+++ Duin/src/Duin/Script/DNLog/Module_DnLog.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "daScript/daScript.h"
+#include "Duin/Core/Debug/DNLog.h"
+//
+//void dn_log_fatal(const char *msg, das::LineInfoArg * at);
+//void dn_log_error(const char *msg, das::LineInfoArg * at);
+//void dn_log_warn(const char *msg, das::LineInfoArg * at);
+//void dn_log_info(const char *msg, das::LineInfoArg * at);
+//void dn_log_trace(const char *msg, das::LineInfoArg * at);
