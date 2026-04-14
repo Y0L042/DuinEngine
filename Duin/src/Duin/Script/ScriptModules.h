eb79f02b2dceb5daf3c52f23d3f916e7deb9bab4
Y0L042@DinkBoekWin11 <weweresomewhere@gmail.com>
2026-04-14 16:30:48 +0200

    Refactor gen_bind.das; add dn_camera module support
    Reorganized includes in ScriptModules.h, adding renderer and camera modules. Updated duin_engine.das_project to support the "dn_camera" module. Refactored gen_bind.das to generalize adapter file generation, removing hardcoded paths and argument parsing, and making it easier to add new adapters.

--- Duin/src/Duin/Script/ScriptModules.h
+++ Duin/src/Duin/Script/ScriptModules.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <module_imgui.h>
 #include "./modules/moduleDNLog/Module_DnLog.h"
 #include "./modules/moduleDoctest/Module_Doctest.h"
+#include "./modules/moduleRenderer/Module_DnRenderer.h"
+#include "./modules/moduleRenderer/Module_DnCamera.h"
 #include "./modules/moduleGameObject/Module_DnGameObject.h"
 #include "./modules/moduleECS/Module_DnECS.h"
 #include "./modules/moduleECS/Module_DnGameWorld.h"
-#include <module_imgui.h>
 //#include "./ECS/Module_DecsEntity.h"
 //#include "./ECS/Module_DecsWorld.h"
 //#include "./ECS/Module_DecsComponents.h"
