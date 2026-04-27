5bf89f90a383033019706af7ff21ed8bb824d11a
Y0L042@DinkBoekWin11 <weweresomewhere@gmail.com>
2026-04-27 12:15:43 +0200

    Add Zeus free camera mode and player state machine
    Introduced a toggleable Zeus developer camera with WASD+mouse controls, speed boost, and vertical movement. Added a generic StateMachine system for managing player states, refactored the Player class to use it, and started with an OnGround_Idle state. Updated input handling and keybind registration, removed old camera logic, improved UI with frame time display, and reorganized module/project structure for better modularity. Updated C++ entry point to use the new project file and required modules.

--- ExampleProjects/DuinFPSDaslang/src/main.cpp
+++ ExampleProjects/DuinFPSDaslang/src/main.cpp
@@ -36,11 +36,13 @@ class DuinFPSDaslangApp : public duin::Application
         duin::PhysicsServer::Get();
         mainScript = CreateChildObject<duin::GameScript>(ENTRY_SCRIPT);
         mainScript->SetDasRoot("C:\\Projects\\CPP_Projects\\Duin\\Duin\\vendor\\daslang");
-        mainScript->SetProjectFile("C:\\Projects\\CPP_Projects\\Duin\\Duin\\duin_engine.das_project");
+        //mainScript->SetProjectFile("C:\\Projects\\CPP_Projects\\Duin\\Duin\\duin_engine.das_project");
+        mainScript->SetProjectFile("C:\\Projects\\CPP_Projects\\Duin\\ExampleProjects\\DuinFPSDaslang\\scripts\\duinfpsdaslang.das_project");
         mainScript->InitModules([]() {
-            NEED_MODULE(Module_DnLiveHost);
             NEED_MODULE(Module_flecs);
             NEED_MODULE(Module_imgui);
+
+            NEED_MODULE(Module_DnLiveHost);
             NEED_MODULE(Module_DnLog);
             NEED_MODULE(Module_DnRenderer);
             NEED_MODULE(Module_DnCamera);
@@ -50,6 +52,7 @@ class DuinFPSDaslangApp : public duin::Application
             NEED_MODULE(Module_DnInput);
             NEED_MODULE(Module_DnPhysicsServer);
             NEED_MODULE(Module_DnCharacterBody);
+            NEED_MODULE(Module_DnApplication);
         });
         mainScript->EnableHotCompile(true, false);
         mainScript->HotCompileAndSimulate();
