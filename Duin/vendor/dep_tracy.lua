e67bdd4830a3c9f8cecb93729b3e3edd342aeee2
Y0L042@DinkBoekUCRT64 <skapeoordiemaan@gmail.com>
2026-06-08 10:00:36 +0200

    Add Lua module for Tracy dependency management
    Introduced `dep_tracy.lua` to manage the Tracy library.
    Implemented `dep_tracy.build()` to handle cloning, fetching,
    and building Tracy with `cmake`. Added runtime library
    configuration based on `Cfg.CRT`. Utilized utility functions
    for command execution and folder management. Included progress
    messages for better build process visibility.

---
+++ Duin/vendor/dep_tracy.lua
@@ -0,0 +1,41 @@
+local Cfg = require "premakeCfg"
+local utils = require "utils"
+local dep_tracy = {}
+local name = "TRACY"
+
+local repo   = "https://github.com/wolfpld/tracy.git"
+local tag    = "v0.13.1"
+local folder = "tracy"
+
+function dep_tracy.build()
+    print("START: " .. name)
+
+    if not os.isdir(folder) then
+        print("\t\tClone")
+        utils.runCommand("git clone --recursive " .. repo .. " " .. folder)
+        utils.runCommand("cd " .. folder .. " && git checkout tags/" .. tag)
+    else
+        print("\t\tFetch")
+        utils.pushDir(folder)
+        utils.runCommand("git stash")
+        utils.runCommand("git pull")
+        utils.runCommand("git checkout tags/" .. tag)
+        utils.popDir()
+    end
+    print(name .. " downloaded.")
+
+    utils.deleteFolder(folder .. "/build_vs2026")
+    local crt_flag = (Cfg.CRT == "MT") and "/MTd" or "/MDd"
+    utils.runCommand('cmake -S ' .. folder .. ' -B ' .. folder .. '/build_vs2026'
+        .. ' -DTRACY_ENABLE=ON'
+        .. ' -DTRACY_ON_DEMAND=ON'
+        .. ' -DBUILD_SHARED_LIBS=OFF'
+        .. ' -DCMAKE_MSVC_RUNTIME_LIBRARY=' .. Cfg.cmake_crt_debug
+        .. ' -DCMAKE_C_FLAGS_DEBUG="' .. crt_flag .. '"'
+        .. ' -DCMAKE_CXX_FLAGS_DEBUG="' .. crt_flag .. '"')
+    utils.runCommand("cmake --build " .. folder .. "/build_vs2026 --config Debug")
+
+    print("END: " .. name)
+end
+
+return dep_tracy
