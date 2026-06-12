5a1d0a21f3c0cf4003c5dbe2cc347fdb18e698ad
Y0L042@DinkBoekUCRT64 <skapeoordiemaan@gmail.com>
2026-06-12 15:45:16 +0200

    Refactor Player and ECS systems; add new features
    Refactored the `Player` class to improve initialization and added hooks for `CharacterBodyDesc` and `MoveSpeed`. Introduced `GravityScale` and `CharacterBodyHandle` components. Enhanced ECS query handling with a new `ecs_query` macro and updated `construct_queries` for better readability. Added `system_integrate_velocity` for velocity integration.
    
    Updated `game_ready` to include `test_system` and removed redundant `_init` calls. Improved `game_draw_ui` with formatted frame time display. Added stricter code options in `dn_ecs_boost.das`. Updated `StateMachine` and `CreateApplication` for better initialization and argument handling.

--- ExampleProjects/DuinFPSDaslang/src/main.cpp
+++ ExampleProjects/DuinFPSDaslang/src/main.cpp
@@ -102,7 +102,7 @@ class DuinFPSDaslangApp : public duin::Application
     }
 };
 
-duin::Application *duin::CreateApplication()
+duin::Application *duin::CreateApplication(int argc, char **argv)
 {
 #ifdef DN_DEBUG
     duin::fs::SetBinDebugMode(true);
